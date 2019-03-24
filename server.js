9072111b630ec4f6b12b645ae1539088f7ae8158
schnibb <schnibbend@gmail.com>
2019-03-24 01:09:55 -0400

    fixing port issue

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.post("/notes", function(req, res) {
 
 
 //listen on port 3000
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
 });
